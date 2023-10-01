702eb9ff3a0814371f31748f934bde981dd83da6
nguyenanh92 <50308802+nguyenanh92@users.noreply.github.com>
2023-10-01 21:27:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const bot = require ('./bot');
+const bot = require('./bot');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 bot.startBot();
+
+
